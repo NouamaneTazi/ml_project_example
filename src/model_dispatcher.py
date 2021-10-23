391a4ac89fbb7c938fa9389765a85b18c1a91861
Nouamane Tazi <nouamane98@gmail.com>
2021-10-23 15:15:03 +0200

    update model_dispatcher

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -2,10 +2,13 @@ from sklearn import tree, ensemble
 
 models = {
     "dt_gini": tree.DecisionTreeClassifier(
-        criterion="gini"
+        criterion="gini",
+        random_state=42
     ),
     "dt_entropy": tree.DecisionTreeClassifier(
-        criterion="entropy"
+        criterion="entropy",
+        random_state=42,
     ),
-    "rf": ensemble.RandomForestClassifier(),
+    "rf": ensemble.RandomForestClassifier(n_jobs=-1,
+                                          random_state=42),
 }
