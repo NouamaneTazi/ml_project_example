ec57acbcd30bc41a4014f5f58581a82f34528644
Nouamane Tazi <nouamane98@gmail.com>
2021-10-23 14:23:35 +0200

    add rf model

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,4 +1,4 @@
-from sklearn import tree
+from sklearn import tree, ensemble
 
 models = {
     "decision_tree_gini": tree.DecisionTreeClassifier(
@@ -7,4 +7,5 @@ models = {
     "decision_tree_entropy": tree.DecisionTreeClassifier(
         criterion="entropy"
     ),
+    "rf": ensemble.RandomForestClassifier(),
 }
