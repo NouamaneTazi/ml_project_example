496a418cc3d8042e67582df3276d85b884885d28
Nouamane Tazi <nouamane98@gmail.com>
2021-10-23 13:08:06 +0200

    add folds creation

---
+++ src/create_folds.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn import model_selection
+
+if __name__ == "__main__":
+    # Read training data
+    df = pd.read_csv("./input/drinking_water_potability.csv")
+
+    # fetch labels
+    y = df["Potability"].values
+
+    # we create a new column called kfold and fill it with -1
+    df["kfold"] = -1
+
+    # shuffle data
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    # initiate the kfold class from model_selection module
+    # we use StratifiedKFold to keep the same % of targets per fold
+    # (because of skewed targets)
+    kf = model_selection.StratifiedKFold(n_splits=5)
+
+    # fill the new kfold column
+    for f, (t_, v_) in enumerate(kf.split(X=df, y=y)):
+        df.loc[v_, 'kfold'] = f
+
+    # save the new csv with kfold column
+    df.to_csv("./input/dri_wat_pot_folds.csv", index=False)
