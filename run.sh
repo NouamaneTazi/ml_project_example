d4988045a3510f678c6877ac38b8189459e0895e
Nouamane Tazi <nouamane98@gmail.com>
2021-10-27 21:37:54 +0200

    quick fix run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # -W ignore : ignore all warnings
-echo "Training on all folds (showing OOF scores)"
-python3 -W ignore src/train.py --fold -1 --model $1 
-echo "Evaluating trained model on whole dataset"
+echo "Training on all folds (showing OOF scores)" && \
+python3 -W ignore src/train.py --fold -1 --model $1 && \
+echo "Evaluating trained model on whole dataset" && \
 python3 -W ignore src/inference.py --model $1
