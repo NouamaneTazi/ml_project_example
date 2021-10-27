b49badd121e5bc0c984f8187d1964428f7671905
Nouamane Tazi <nouamane98@gmail.com>
2021-10-27 12:11:29 +0200

    add preprocessing params to model_dispatcher

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,19 +1,31 @@
 from sklearn import tree, ensemble
 import xgboost as xgb
 
+# TODO: hyperparameter tuning for each model.
 models = {
-    "dt_gini": tree.DecisionTreeClassifier(
-        criterion="gini",
-        random_state=42
-    ),
-    "dt_entropy": tree.DecisionTreeClassifier(
-        criterion="entropy",
-        random_state=42,
-    ),
-    "rf": ensemble.RandomForestClassifier(n_jobs=-1,
-                                          random_state=42),
-    "xgb": xgb.XGBClassifier(
-        n_jobs=-1,
-        random_state=42
-    )
+    "dt_gini": {
+        "model": tree.DecisionTreeClassifier(
+            criterion="gini",
+            random_state=42
+        ),
+        "preprocessing_params":  {"missing": "median", "scaling": "standard", "add_Solids_log": True, "poly_degree": 1}
+    },
+    "dt_entropy": {
+        "model":  tree.DecisionTreeClassifier(
+            criterion="entropy",
+            random_state=42,
+        ),
+    },
+    "rf": {
+        "model": ensemble.RandomForestClassifier(n_jobs=-1,
+                                                 random_state=42),
+        "preprocessing_params":  {"add_Solids_log": False}
+    },
+    "xgb": {
+        "model":  xgb.XGBClassifier(
+            n_jobs=-1,
+            random_state=42
+        ),
+        "preprocessing_params":  {"add_Solids_log": False}
+    },
 }
